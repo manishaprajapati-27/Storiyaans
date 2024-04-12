0b088d6a10ca4f2f7ff1497794c7bdb3455af8a4
Manisha <manprajapati268@gmail.com>
2024-04-13 00:02:17 +0530

    added auth middlewares

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/register").post(registerUser);
 
 export default router;
